ed53633beb2289dff3d896fc7eafb1f09d6d3ac0
ilyascant <ilyascant123@gmail.com>
2024-02-28 16:10:59 +0300

    slider bug fix

--- client/src/components/SliderCard.jsx
+++ client/src/components/SliderCard.jsx
@@ -23,67 +23,65 @@ const SliderCard = ({ windowSize, name, items, images, loading }) => {
     infinite: true,
     autoplay: true,
     speed: 500,
-    slidesToShow:
-      items?.length >= 3
-        ? windowSize.width > 600
-          ? 3
-          : windowSize.width > 450
-          ? 2
-          : 1
-        : items?.length > 1 && windowSize.width > 450
-        ? 2
-        : items?.length,
-
+    slidesToShow: windowSize.width > 600 ? 3 : windowSize.width > 450 ? 2 : 1,
     slidesToScroll: windowSize.width > 600 ? 3 : windowSize.width > 450 ? 2 : 1,
     prevArrow: <CustomPrevArrow />,
     nextArrow: <CustomNextArrow />,
   };
+
+  const element = (item, index) => (
+    <div className="relative xs:h-36 h-44 outline-none" key={index}>
+      <a
+        name={Math.random() * 100}
+        ref={(myRefs.current[index] ??= { current: null })}
+        className="absolute inset-0 w-full h-full"
+        href={item?.link}
+        target="_blank"></a>
+      <div
+        onClick={() => {
+          myRefs.current[index].current.click();
+        }}
+        className="cursor-pointer relative flex h-full border rounded-lg shadow-md mx-2 overflow-hidden">
+        <img
+          src={images?.length >= index ? images[index]?.image : item?.image}
+          alt=""
+          draggable="false"
+          className=" z-10 absolute w-full h-full inset-0 object-cover"
+        />
+        <div className="w-full h-[30%] z-20 mt-auto backdrop-blur-md bg-black/20">
+          <p className="mb-1 text-white text-sm text-ellipsis overflow-hidden">
+            {(() => {
+              const splitted = item?.title.split(" ");
+              let count = 0;
+              let allCount = 0;
+              let string = splitted.map((el, index) => {
+                allCount += el.length;
+                if (count < 24) {
+                  count += el.length;
+                  return el;
+                }
+              });
+              string = string.join(" ");
+              if (count < allCount) string += " ...";
+              return string;
+            })()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     !loading && (
       <div className="relative xl:w-[40%] lg:w-[50%] w-full mt-4 mb-16">
         <h2 className="font-semibold mb-2">{name}</h2>
-        <Slider className="px-4" ref={sliderRef} {...settings}>
-          {items?.map((item, index) => (
-            <div className="relative xs:h-36 h-44 outline-none" key={index}>
-              <a
-                name={Math.random() * 100}
-                ref={(myRefs.current[index] ??= { current: null })}
-                className="absolute inset-0 w-full h-full"
-                href={item?.link}
-                target="_blank"></a>
-              <div
-                onClick={() => {
-                  myRefs.current[index].current.click();
-                }}
-                className="cursor-pointer relative flex h-full border rounded-lg shadow-md mx-2 overflow-hidden">
-                <img
-                  src={images?.length >= index ? images[index]?.image : item?.image}
-                  alt=""
-                  className="z-10 absolute w-full h-full inset-0 object-cover"
-                />
-                <div className="w-full h-[30%] z-20 mt-auto backdrop-blur-md bg-black/20">
-                  <p className="mb-1 text-white text-sm text-ellipsis overflow-hidden">
-                    {(() => {
-                      const splitted = item?.title.split(" ");
-                      let count = 0;
-                      let allCount = 0;
-                      let string = splitted.map((el, index) => {
-                        allCount += el.length;
-                        if (count < 24) {
-                          count += el.length;
-                          return el;
-                        }
-                      });
-                      string = string.join(" ");
-                      if (count < allCount) string += " ...";
-                      return string;
-                    })()}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Slider>
+        {items?.length < 2 ? (
+          element(items[0], 0)
+        ) : (
+          <Slider className="px-4" ref={sliderRef} {...settings}>
+            {items?.map((item, index) => element(item, index))}
+          </Slider>
+        )}
       </div>
     )
   );
